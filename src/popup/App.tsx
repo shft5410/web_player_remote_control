b13d4433dc4a0ceed3d5e20450648c7b35eb66ef
Simon Hammer <77269703+shft5410@users.noreply.github.com>
2025-06-14 23:14:52 +0200

    Move popup files except the entry files to src/popup

---
+++ src/popup/App.tsx
@@ -0,0 +1,13 @@
+import '@/popup/App.css'
+import RippleBackground from '@/popup/components/RippleBackground'
+
+function App() {
+    return (
+        <>
+            <RippleBackground />
+            <h1>Hello World!</h1>
+        </>
+    )
+}
+
+export default App
