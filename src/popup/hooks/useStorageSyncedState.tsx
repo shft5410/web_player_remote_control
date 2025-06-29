6ec27af9279debadff21a619700d777d8093bbbb
Simon Hammer <77269703+shft5410@users.noreply.github.com>
2025-06-29 16:23:56 +0200

    Fix bug causing flickering of storage value

--- src/popup/hooks/useStorageSyncedState.tsx
+++ src/popup/hooks/useStorageSyncedState.tsx
@@ -12,31 +12,41 @@ export default function useStorageSyncedState<T>(
     const activeTabUrl = useActiveTabUrl()
 
     const [value, setValue] = React.useState<T>(initialValue)
-    const isValueInitialized = React.useRef(false)
-    const debounceTimeout = React.useRef<NodeJS.Timeout | null>(null)
+    const [isLoaded, setIsLoaded] = React.useState<boolean>(false)
+
+    const pageSpecificStorageKey = React.useMemo<StorageItemKey | null>(() => {
+        if (!activeTabUrl || !activeTabUrl.origin) {
+            return null
+        }
+        return `local:page:${activeTabUrl.origin}/${storageKey}`
+    }, [activeTabUrl?.origin, storageKey])
 
     React.useEffect(() => {
-        if (activeTabUrl) {
-            const pageSpecificStorageKey: StorageItemKey = `local:page:${activeTabUrl.origin}/${storageKey}`
-            if (!isValueInitialized.current) {
-                isValueInitialized.current = true
-                const initialValue = value
-                storage.getItem(pageSpecificStorageKey).then((storedValue) => {
-                    if (typeGuard(storedValue)) {
-                        setValue((prevValue) => (prevValue === initialValue ? storedValue : prevValue))
-                    }
-                })
-            } else {
-                if (debounceTimeout.current) {
-                    clearTimeout(debounceTimeout.current)
+        setIsLoaded(false)
+        if (pageSpecificStorageKey) {
+            const initialValue = value
+            storage.getItem(pageSpecificStorageKey).then((storedValue) => {
+                if (typeGuard(storedValue)) {
+                    setValue((prevValue) => (prevValue === initialValue ? storedValue : prevValue))
+                    setIsLoaded(true)
                 }
-                debounceTimeout.current = setTimeout(() => {
-                    storage.setItem(pageSpecificStorageKey, value)
-                    debounceTimeout.current = null
-                }, storageDebounceTime)
-            }
+            })
         }
-    }, [activeTabUrl, value, storageKey])
+    }, [pageSpecificStorageKey])
+
+    React.useEffect(() => {
+        let debounceTimeout: NodeJS.Timeout | null = null
+        if (isLoaded && pageSpecificStorageKey) {
+            debounceTimeout = setTimeout(() => {
+                storage.setItem(pageSpecificStorageKey, value)
+                debounceTimeout = null
+            }, storageDebounceTime)
+        }
+        return () => {
+            if (!debounceTimeout) return
+            clearTimeout(debounceTimeout)
+        }
+    }, [isLoaded, pageSpecificStorageKey, value])
 
     return [value, setValue]
 }
