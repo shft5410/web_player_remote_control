602c8b94adf96466966d8933e960a823c9d3ffbc
Simon Hammer <77269703+shft5410@users.noreply.github.com>
2025-07-02 20:42:42 +0200

    Listen for changes of the active tab

--- src/popup/hooks/useActiveTab.tsx
+++ src/popup/hooks/useActiveTab.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import { browser, type Browser } from '#imports'
 
-export default function useActiveTab(): Browser.tabs.Tab | null {
+export default function useActiveTab(watchProperties?: string[]): Browser.tabs.Tab | null {
     const [activeTab, setActiveTab] = React.useState<Browser.tabs.Tab | null>(null)
 
     React.useEffect(() => {
         browser.tabs.query({ active: true, currentWindow: true }).then(([activeTab]) => {
-            if (activeTab) {
-                setActiveTab(activeTab)
-            }
+            if (!activeTab) return
+            setActiveTab(activeTab)
         })
     }, [])
+
+    React.useEffect(() => {
+        function handleActiveTabChange(_tabId: number, changeInfo: Browser.tabs.TabChangeInfo, tab: Browser.tabs.Tab) {
+            console.log('Tab updated:', changeInfo)
+            if (!tab.active || watchProperties?.every((property) => !(property in changeInfo))) return
+            browser.windows.getCurrent().then((currentWindow) => {
+                if (tab.windowId !== currentWindow.id) return
+                setActiveTab(tab)
+            })
+        }
+
+        browser.tabs.onUpdated.addListener(handleActiveTabChange)
+
+        return () => {
+            browser.tabs.onUpdated.removeListener(handleActiveTabChange)
+        }
+    }, [JSON.stringify(watchProperties)])
 
     return activeTab
 }
